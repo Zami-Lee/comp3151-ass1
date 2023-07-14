d8eab6cd239e2daabd3fb76ba8e2e8dbc2ae3bd5
Zami-Lee <zlee5358@gmail.com>
2023-07-14 13:39:27 +1000

    fix cleanup when inserting

--- lib/src/test/java/TestCleanup2.java
+++ lib/src/test/java/TestCleanup2.java
@@ -28,6 +28,7 @@ public class TestCleanup2 {
                 System.out.println("Deleting: " + i);
                 a1.delete(i);
             }
+            a1.printArray();
         });
 
         Thread thread3 = new Thread(() -> {
@@ -56,6 +57,112 @@ public class TestCleanup2 {
         }
 
         int[] expected = {-1, -1, -1, -1, -1, 1, 3, 5, 7, 9};
+        assertEquals(Arrays.toString(expected), Arrays.toString(a1.getArray()));
+    }
+
+    @Test
+    public void testCleanupUpTo() throws InterruptedException {
+        UNSWArray a1 = new UNSWArray(11);
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Inserting: " + i);
+                a1.insert(i);
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            try {
+                // allow all inserts first
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+
+            }
+            for (int i = 0; i < 10; i += 2) {
+                System.out.println("Deleting: " + i);
+                a1.delete(i);
+            }
+        });
+
+        Thread thread3 = new Thread(() -> {
+            try {
+                // allow all inserts and deletes first
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+
+            }
+            for (int i = 1; i < 10; i += 2) {
+                assertTrue(a1.member(i));
+            }
+            a1.insert(6);
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+        } catch (InterruptedException e) {
+
+        }
+
+        int[] expected = {-1, -1, -1, -1, 1, 3, 5, 6, -1, 7, 9};
+        assertEquals(Arrays.toString(expected), Arrays.toString(a1.getArray()));
+    }
+
+    @Test
+    public void testCleanupUpToAll() throws InterruptedException {
+        UNSWArray a1 = new UNSWArray(11);
+
+        Thread thread1 = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("Inserting: " + i);
+                a1.insert(i);
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            try {
+                // allow all inserts first
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+
+            }
+            for (int i = 0; i < 10; i += 2) {
+                System.out.println("Deleting: " + i);
+                a1.delete(i);
+            }
+        });
+
+        Thread thread3 = new Thread(() -> {
+            try {
+                // allow all inserts and deletes first
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+
+            }
+            for (int i = 1; i < 10; i += 2) {
+                assertTrue(a1.member(i));
+            }
+            a1.insert(11);
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+            thread3.join();
+        } catch (InterruptedException e) {
+
+        }
+
+        int[] expected = {-1, -1, -1, -1, -1, 1, 3, 5, 7, 9, 11};
         assertEquals(Arrays.toString(expected), Arrays.toString(a1.getArray()));
     }
 
